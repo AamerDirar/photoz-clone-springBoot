5cc77932be2d57ba7e205abbcf0e7a52751b9490
AamerDirar <aamer.idris91@gmail.com>
2024-09-25 11:28:09 +0300

    upload photo endpoint

--- src/main/java/com/jetbrains/marco/photoz/clone/PhotozController.java
+++ src/main/java/com/jetbrains/marco/photoz/clone/PhotozController.java
@@ -2,16 +2,12 @@ package com.jetbrains.marco.photoz.clone;
 
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.util.*;
 
 @RestController
 public class PhotozController {
@@ -42,6 +38,16 @@ public class PhotozController {
         Photo photo = db.remove(id);
         if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 
+    }
+
+    @PostMapping("/photoz")
+    public Photo create(@RequestPart("data") MultipartFile file) throws IOException {
+        Photo photo = new Photo();
+        photo.setId(UUID.randomUUID().toString());
+        photo.setFileName(file.getOriginalFilename());
+        photo.setData(file.getBytes());
+        db.put(photo.getId(), photo);
+        return photo;
     }
 
 }
